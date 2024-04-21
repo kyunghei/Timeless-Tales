287adfc52c266d6d7d1eaa65e951d5bce76b1c80
ShinyMyst <shinymyst@gmail.com>
2024-04-20 20:19:26 -0400

    Added basic outline for set tags file

---
+++ backend/set_choice_tags.py
@@ -0,0 +1,19 @@
+# This file determines what types of choices should be offered at each story beat
+
+# TODO
+# Generate choice tags
+def  get_choice_tags(current_tags: list[str]):
+    """Given a list of the tags associated with most recent choice.
+    Returns three lists containing the tags for the three future choices."""
+    pass
+    # TODO - Use story length variables to assign climax tag
+
+
+# TODO
+# Function that removes the tags from the API's response.
+# We need method to store the tags associated with the user's choice as well
+
+# TODO
+# Create two test functions
+# Test function 1 - tests 1000s of stories and lists tag frequency across all 10k
+# Test function 2- tests a single story and the flow of tags
