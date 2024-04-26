f10aa595645bdeae51dfa895aff026001892faad
ShinyMyst <shinymyst@gmail.com>
2024-04-25 23:47:30 -0400

    Basic notes about what to do

---
+++ backend/prompt_generator.py
@@ -0,0 +1,8 @@
+def get_prompt():
+    # Apply tags to the prompt
+    # Consider previous tags
+    # Figure out if we're at climax or about to reach the climax?
+        # Make climax dynamic based on intensity?
+    # Consider intensity?  and proximity to climax
+    # Generate decision by referencing the future tags
+    
