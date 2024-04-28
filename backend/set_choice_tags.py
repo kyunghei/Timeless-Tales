8d25cdb52a6791dc0c2f1a202b77a8c9f4c63290
ShinyMyst <shinymyst@gmail.com>
2024-04-28 12:43:53 -0400

    Fixed linting issues.

--- backend/set_choice_tags.py
+++ backend/set_choice_tags.py
@@ -11,26 +11,26 @@ json_path = os.path.join(script_dir, 'tags.json')
 
 
 def get_choice_tags(current_tags: list[str], num_choices) -> list[set[str]]:
-    """ This function is utilized to determine the tags options for next story beat.
-    Given a singular list of tags, returns a list with multiple sets of tags."""
+    """ Determines the tag options for next story beat."""
     # TODO - Here is where JSON needs to be reimplemented
     # Prepare JSON Data
     with open(json_path, 'r') as file:
         json_data = json.load(file)
-    
+
     # Determine the combined weight of all options
-    result_weight = dict()  
+    result_weight = dict()
     for tag in current_tags:
-        # Each current tag has its own base_weight dict giving the base odds of each result
-        base_weight = json_data[tag]  
+        # Each current tag has its own base_weight dict
+        # This gives base odds of each result
+        base_weight = json_data[tag]
         result_weight = _adjust_weight(base_weight, result_weight)
-   
+
     # Determine Resulting Tags
     all_choices = []
     keys = list(result_weight.keys())
     weights = list(result_weight.values())
     for _ in range(num_choices):
-        all_choices.append(_generate_choice(keys, weights))   
+        all_choices.append(_generate_choice(keys, weights))
 
     return all_choices
 
@@ -42,10 +42,10 @@ def _adjust_weight(base_weight, result_weight):
             result_weight[tag_name] += base_weight[tag_name]
         else:
             result_weight[tag_name] = base_weight[tag_name]
-    
-    return result_weight    
 
-                 
+    return result_weight
+
+
 def _generate_choice(keys, weights) -> set:
     """Helper function that returns the tag(s) for ONE choice."""
     choice = []
@@ -60,7 +60,7 @@ def _generate_choice(keys, weights) -> set:
 
 
 def test_tag_frequency(num_iterations, story_length, num_choices):
-    """Counts the number of times each tag appears in multiple simulated stories."""
+    """Counts frequency of each tag appears in multiple simulated stories."""
     frequency_count = dict()
     i = 0
     while i < num_iterations:
@@ -70,12 +70,12 @@ def test_tag_frequency(num_iterations, story_length, num_choices):
             frequency_count = _update_frequency(frequency_count, current_tags)
             # Determine next choice
             choices = get_choice_tags(current_tags, num_choices)
-            current_tags = choices[random.randint(0,2)]
+            current_tags = choices[random.randint(0, 2)]
             n += 1
         i += 1
     print(frequency_count)
 
-    
+
 def _update_frequency(frequency_count, current_tags):
     """Helper that counts how often tag appears."""
     for tag in current_tags:
@@ -84,7 +84,7 @@ def _update_frequency(frequency_count, current_tags):
         else:
             frequency_count[tag] = 0
     return frequency_count
-    
+
 
 def test_tag_selection(story_length, num_choices):
     """Prints out a potential set of tags in the order they were selected."""
@@ -102,10 +102,10 @@ def main():
     iterations = 10
     story_length = 10
     num_choices = 3
-    
-    test_tag_frequency(iterations, story_length, num_choices)        
+
+    test_tag_frequency(iterations, story_length, num_choices)
     test_tag_selection(story_length, num_choices)
-    
+
 
 if __name__ == "__main__":
     main()
