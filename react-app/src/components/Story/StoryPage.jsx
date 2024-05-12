f5df3fa95350af666d73ad306a784ef41b0f1bac
Kyung Hei Seo <seoky@oregonstate.edu>
2024-05-11 21:19:46 -0700

    implement loading state while waiting for story beat to be fetched from backend

--- react-app/src/components/Story/StoryPage.jsx
+++ react-app/src/components/Story/StoryPage.jsx
@@ -15,39 +15,52 @@ function StoryPage({ selectedGenre, selectedName, selectedAvatar, selectedLength
     // State to save the current story beat data (story_text, story_image, current_beat, current_lives) sent from backend
     const [currentBeatData, setCurrentBeatData] = useState(null);
 
+    // State to track loading status
+    const [isLoading, setIsLoading] = useState(true);
+
     // Bool state that controls whether to show story text or story choices
     const [showChoices, setShowChoices] = useState(false);
 
     // Fetch first story beat data from backend when component mounts.
     useEffect(() => {
+        setIsLoading(true); // Loading before sending request
+
         async function fetchFirstBeat() {
             try {
                 const res = await axios.post('/post-story-beat');
                 setCurrentBeatData(res.data);
+                setIsLoading(false); // Remove loading after receipt of data
             } catch (error) {
                 console.error('Error fetching first story beat:', error);
+                setIsLoading(false);
             }
         }
         fetchFirstBeat();
     }, []);
 
-    // TODO: Add some conditional for loading state here
 
     function handleNext() {
         setShowChoices(!showChoices);
     }
 
-
     return (
-        <div>
-            <StoryBackgroundImage genre={currentBeatData.genre} />
-            <AvatarDisplay name={selectedName} avatar={selectedAvatar} genre={selectedGenre} />
-            <AvatarLife genre={selectedGenre} lives={currentBeatData} />
-            <StoryBeatText text={currentBeatData.story_text} />
-            <StoryBeatImage imageUrl={currentBeatData.story_image} />
-            <ProgressBar currentBeat={currentBeatData.current_beat} maxBeat={selectedLength} />
-            <StoryButton onClick={handleNext} genre={selectedGenre} lives={currentBeatData.current_lives} />
-        </div>
+        <>
+            {isLoading ? (
+                <div>Loading...</div>
+            ) : (
+                <div>
+                    <StoryBackgroundImage genre={currentBeatData.genre} />
+                    <AvatarDisplay name={selectedName} avatar={selectedAvatar} genre={selectedGenre} />
+                    <AvatarLife genre={selectedGenre} lives={currentBeatData} />
+                    <StoryBeatText text={currentBeatData.story_text} />
+                    <StoryBeatImage imageUrl={currentBeatData.story_image} />
+                    <ProgressBar currentBeat={currentBeatData.current_beat} maxBeat={selectedLength} />
+                    <StoryButton onClick={handleNext} genre={selectedGenre} lives={currentBeatData.current_lives} />
+                </div>
+            )}
+
+        </>
+
     );
 }
 
