2bbf4a0aa034e02330175e50baf5be835ece7469
Abby Thornton <thorntab@oregonstate.edu>
2024-05-12 15:58:34 -0500

    Added text to accordians

--- react-app/src/components/Home/Accordion.jsx
+++ react-app/src/components/Home/Accordion.jsx
@@ -5,10 +5,36 @@ import AccordionItem from './AccordionItem';
 function Accordion() {
     return (
         <div>
-            <AccordionItem header="+ Customize Your Adventure" body="Blah" />
-            <AccordionItem header="+ Dive into the Story" body="Blah" />
-            <AccordionItem header="+ Make Your Choices" body="Blah" />
-            <AccordionItem header="+ Play Through a New Story" body="Blah" />
+            <AccordionItem header="+ Customize Your Adventure" 
+            body="Get ready to embark on your very own interactive storytelling adventure.
+            Here's how to customize your journey:\n\n
+            <ul>
+                <li>Choose your Genre: Select from three exciting story genres - Fantasy, Western, or Sci-Fi.</li>
+                <li>Select Story Length: Decide how long you want your adventure to be - Short, Medium, or Long.</li>
+                <li>Create your Character: Pick an avatar and give your character a name to represent you in the story.</li>
+            </ul>" />
+            
+            <AccordionItem header="+ Dive into the Story" 
+            body="Now that you've customized your adventure, it's time to dive into the story! Sit back, relax, and let the AI
+            weave a captivating tale filled with twists and turns." />
+            
+            <AccordionItem header="+ Make Your Choices" 
+            body="As the story unfolds, you'll be presented with various choices that will shape the outcome of your adventure. 
+            Choose wisely, as some decisions may lead to unexpected consequences! Remember, each character has only three lives, 
+            so be cautious with your choices.
+            <ul>
+                <li>Select a Choice: Read through the options presented and choose the one you think is best for your character.</li>
+                <li>Face the Consequences: Some choices may lead to a 'bad end' and result in the loss of one of your character's lives.</li>
+            </ul>" />
+            
+            <AccordionItem header="+ Play Through a New Story" 
+            body="Once your adventure has concluded, you can start anew with different customization options or replay the same story 
+            to explore alternative paths.\n\n
+
+            <ul>
+                <li>Revisit Customization: Choose to replay with the same genre, story length, and character, or mix things up with new options.</li>
+                <li>Explore New Paths: Experiment with different choices to uncover alternative storylines and outcomes.</li>
+            </ul>" />
         </div>
     );
 }
