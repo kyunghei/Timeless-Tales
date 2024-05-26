79e6561b4c23627d1d894a2a8223aeff3b52f069
Abby Thornton <thorntab@oregonstate.edu>
2024-05-26 12:23:15 -0500

    Updated ProgressBar to StoryProgressBar on StoryPage:177. Missed the change in a previous merge update.

--- react-app/src/components/Story/StoryPage.jsx
+++ react-app/src/components/Story/StoryPage.jsx
@@ -174,7 +174,7 @@ function StoryPage({ selectedGenre, selectedName, selectedAvatar, selectedLength
                                 <StoryBeatChoices choices={[currentBeatData.choice_1, currentBeatData.choice_2, currentBeatData.choice_3]} userChoiceHandler={handleUserChoice} /> :
                                 <StoryBeatText text={currentBeatData.story_text} />}
                             
-                            <ProgressBar currentBeat={currentBeatData.current_beat} maxBeat={selectedLength} />
+                            <StoryProgressBar currentBeat={currentBeatData.current_beat} maxBeat={selectedLength} />
                             
                             {/* Displays correct button */}
                             {currentBeatData.current_lives == 0 ?
